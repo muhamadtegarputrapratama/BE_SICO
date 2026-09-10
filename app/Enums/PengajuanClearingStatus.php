056401b52beda2d24b8182211c4adb3da935b729
arlin <arlinnurliani452@gmail.com>
2026-09-10 10:20:32 +0700

    update enums dan pengajuan service

--- app/Enums/PengajuanClearingStatus.php
+++ app/Enums/PengajuanClearingStatus.php
@@ -7,6 +7,7 @@ enum PengajuanClearingStatus: string
     case DIAJUKAN = 'menunggu';
     case REVISI_ADMIN = 'revisi_admin';
     case DIVERIFIKASI_ADMIN = 'diverifikasi_admin'; //menunggu atasan menyetujui
+    case DITOLAK = 'ditolak';
     case DISETUJUI = 'disetujui'; //disetujui atasan
 
     public function label(): string 
@@ -15,13 +16,14 @@ enum PengajuanClearingStatus: string
             self::DIAJUKAN => 'Menunggu', 
             self::REVISI_ADMIN => 'Perlu revisi admin',
             self::DIVERIFIKASI_ADMIN => 'Menunggu persetujuan atasan',
+            self::DITOLAK => 'Ditolak', 
             self::DISETUJUI => 'Disetujui',
         };
     }
 
-    public function isFinal(): bool 
+    public function isFinal(): bool
     {
-        return $this === self::DISETUJUI;
+        return in_array($this, [self::DISETUJUI, self::DITOLAK]);
     }
 
 }
