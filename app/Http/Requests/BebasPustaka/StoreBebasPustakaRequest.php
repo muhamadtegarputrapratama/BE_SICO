6c7079cb55725e64ddcb373e00a915f4c04a0029
muhamadtegarputrapratama <muhamadtegarputrapratama@smkwikrama.sch.id>
2026-09-21 20:19:26 +0700

    fix:store bebas pustaka

--- app/Http/Requests/BebasPustaka/StoreBebasPustakaRequest.php
+++ app/Http/Requests/BebasPustaka/StoreBebasPustakaRequest.php
@@ -14,23 +14,11 @@ class StoreBebasPustakaRequest extends FormRequest
     public function rules(): array
     {
         return [
-            // (ubah ke 20480 untuk 20 MB)
-            'file_skripsi' => ['required', 'file', 'mimes:pdf', 'max:10240'],
+            'file_skripsi' => ['required', 'file', 'mimes:pdf', 'max:5120'],
         ];
     }
 
     public function messages(): array
-    {
-        return [
-            'file_skripsi.required' => 'File skripsi wajib diunggah.',
-            'file_skripsi.file' => 'File skripsi tidak valid.',
-            'file_skripsi.mimes' => 'File skripsi harus berformat PDF.',
-            'file_skripsi.max' => 'Ukuran file skripsi maksimal 10 MB.',
-            'file_skripsi.uploaded' => 'File gagal diunggah. Ukuran melebihi batas server.',
-        ];
-    }
-
-     public function messages(): array
     {
         return [
             'file_skripsi.required' => 'File skripsi wajib diunggah.',
